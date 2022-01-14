26b9e800cedce45ff42c7be8f90f4abf517e39dc
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-14 10:28:10 -0800

    Continue basic component structure, fetch data

---
+++ src/components/ParkCard.js
@@ -0,0 +1,9 @@
+const ParkCard = () => {
+  return(
+    <section>
+      <h2>Joshua Tree</h2>
+    </section>
+  )
+}
+
+export default ParkCard;
--- src/components/ParkContainer.js
+++ src/components/ParkContainer.js
@@ -1,11 +1,35 @@
+import { useState, useEffect } from 'react';
 import ActivityForm from './ActivityForm';
+import ParkCard from './ParkCard';
 
 const ParkContainer = () => {
+  const [parks, setParks] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [bucketList, setBucketList] = useState([]);
+
+  const fetchData = async (url, state) => {
+    try {
+      const fetchedData = await fetch(`https://developer.nps.gov/api/v1/${url}api_key=hBaA4GZsEXWedcUXnVCAvMCWhxf5u2Jp0z9gPDRy`)
+      const { data } = await fetchedData.json()
+      state === 'parks' ? setParks(data) : setActivities(data)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData('parks?stateCode=CA&', 'parks')
+    fetchData('activities?', 'activities')
+  })
+
   return(
-    <section>
+    <main>
       <ActivityForm />
-      <section>parks</section>
-    </section>
+      <section>
+        <ParkCard />
+      </section>
+      {/* <ParkPage /> */}
+    </main>
   )
 }
 
