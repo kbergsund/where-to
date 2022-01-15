376f8e208e30db88b125d5ec19c7e20816fd6358
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-14 17:12:00 -0800

    Finalize fetch calls & data formatting

--- src/components/ParkContainer.js
+++ src/components/ParkContainer.js
@@ -12,23 +12,19 @@ const ParkContainer = () => {
     try {
       const fetchedData = await fetch(`https://developer.nps.gov/api/v1/${url}api_key=hBaA4GZsEXWedcUXnVCAvMCWhxf5u2Jp0z9gPDRy`)
       const { data } = await fetchedData.json()
-      state === 'parks' ? formatParkData(data): setActivities(data)
+      state === 'parks' ? setParks(data.map(park => {
+        return {
+          fullName: park.fullName,
+          url: park.url,
+          description: park.description,
+          activities: park.activities.map(activity => activity.name),
+          images: park.images
+        }
+      })) : 
+      setActivities(data.map(activity => activity.name))
     } catch (err) {
       console.log(err)
     }
-  }
-
-  const formatParkData = (data) => {
-    const formattedParks = data.map(park => {
-      return {
-        fullName: park.fullName,
-        url: park.url,
-        description: park.description,
-        activities: park.activities.map(activity => activity.name),
-        images: park.images
-      }
-      setParks(formattedParks)
-    })
   }
 
   useEffect(() => {
