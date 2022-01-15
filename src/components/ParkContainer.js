ea2ca72e2566a11dc8b9609116ee2c150afa1595
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-15 15:22:35 -0800

    Continue styling

--- src/components/ParkContainer.js
+++ src/components/ParkContainer.js
@@ -50,11 +50,11 @@ const ParkContainer = () => {
 
   return (
     <main>
-      {/* <ActivityForm activities={activities} filterParks={filterParks}/>
+      <ActivityForm activities={activities} filterParks={filterParks}/>
       <section className='park-container'>
         {displayParkCards}
-      </section> */}
-      <ParkPage park={parks[0]}/>
+      </section>
+      {parks.length && <ParkPage park={parks[0]}/>}
     </main>
   )
 }
--- src/components/ParkPage.js
+++ src/components/ParkPage.js
@@ -1,16 +1,16 @@
 import '../scss/ParkPage.scss';
 
 const ParkPage = ({ park }) => {
-  console.log(park.fullName)
+  // console.log(park.fullName)
   return (
     <section className='park-page'>
       <div className='left-side-content'>
         <h2>{park.fullName}</h2>
         <button>Add to Bucket List</button>
         <p>{park.description}</p>
-        <div className='bottom-btns'>
-          <button>Read More</button>
-        </div>
+        {/* <div className='bottom-btns'> */}
+          <button className='read-more-btn'>Read More</button>
+        {/* </div> */}
       </div>
       {/* <div className='right-side-img'> */}
       <img src={park.images[1].url} alt={park.images[1].altText}></img>
