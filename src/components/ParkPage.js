d8519c571d6e6c70e852edb6b95d2e379d0f398e
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-15 20:25:00 -0800

    Add activities to ParkPage, polish desktop styling

--- src/components/ParkPage.js
+++ src/components/ParkPage.js
@@ -1,21 +1,22 @@
 import '../scss/ParkPage.scss';
 
 const ParkPage = ({ park }) => {
-  // console.log(park.fullName)
+  const displayActivities = park.activities.map(activity => <p className='activity'>{activity}</p>)
+
   return (
     <section className='park-page'>
       <div className='left-side-content'>
-        <div className='bottom-btns'>
-        <h2 className='park-page-title'>{park.fullName}</h2>
-        <button className='park-page-add-btn'>Add to Bucket List</button>
+        <div className='title-btn'>
+          <h2 className='park-page-title'>{park.fullName}</h2>
+          <button className='park-page-add-btn'>Add to Bucket List</button>
+        </div>
+        <div className='activity-container'>
+          {displayActivities}
         </div>
         <p>{park.description}</p>
         <a href={park.url}>Learn More</a>
       </div>
-      {/* <div className='right-side-img'> */}
       <img src={park.images[1].url} alt={park.images[1].altText}></img>
-      {/* </div> */}
-
     </section>
   )
 }
