ed6cb57d49994d57b0586fe8f79756cf8d9481cb
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-15 20:32:36 -0800

    Debug ParkPage display

--- src/components/ParkPage.js
+++ src/components/ParkPage.js
@@ -3,24 +3,24 @@ import '../scss/ParkPage.scss';
 
 const ParkPage = ({ parks }) => {
   const clickedParkCode = useParams().parkCode;
-  const ClickedPark = parks.find(park => park.parkCode === clickedParkCode)
+  const clickedPark = parks.find(park => park.parkCode === clickedParkCode)
   
-  const displayActivities = ClickedPark.activities.map(activity => <p className='activity'>{activity}</p>)
+  const displayActivities = clickedPark.activities.map(activity => <p className='activity'>{activity}</p>)
 
   return (
     <section className='park-page'>
       <div className='left-side-content'>
         <div className='title-btn'>
-          <h2 className='park-page-title'>{ClickedPark.fullName}</h2>
+          <h2 className='park-page-title'>{clickedPark.fullName}</h2>
           <button className='park-page-add-btn'>Add to Bucket List</button>
         </div>
         <div className='activity-container'>
           {displayActivities}
         </div>
-        <p>{ClickedPark.description}</p>
-        <a href={park.url}>Learn More</a>
+        <p>{clickedPark.description}</p>
+        <a href={clickedPark.url}>Learn More</a>
       </div>
-      <img src={ClickedPark.images[1].url} alt={ClickedPark.images[1].altText}></img>
+      <img src={clickedPark.images[1].url} alt={clickedPark.images[1].altText}></img>
     </section>
   )
 }
